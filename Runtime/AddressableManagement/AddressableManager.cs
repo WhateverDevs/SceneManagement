ad16f136e78e229db2e8420c48f8f0001f062f06
Rodrigo Varga <varguiniano@gmail.com>
2021-05-20 16:55:32 +0200

    - Some simple testing.

--- Runtime/AddressableManagement/AddressableManager.cs
+++ Runtime/AddressableManagement/AddressableManager.cs
@@ -7,12 +7,20 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceLocations;
 using WhateverDevs.Core.Runtime.Common;
+using Zenject;
+using Version = WhateverDevs.Core.Runtime.Build.Version;
 
 namespace WhateverDevs.SceneManagement.Runtime.AddressableManagement
 {
     [CreateAssetMenu(menuName = "WhateverDevs/SceneManagement/AddressableManager", fileName = "AddressableManager")]
     public class AddressableManager : LoggableScriptableObject<AddressableManager>, IAddressableManager
     {
+        public AddressableVersionDependence AddressableVersionDependence;
+        
+        [Inject]
+        [HideInInspector]
+        public Version Version;
+        
         [Button]
         [EnableIf("@UnityEngine.Application.isPlaying")]
         public void CheckAvailableAddressables(Action<AddressableStateReport> callback)
@@ -25,16 +33,19 @@ namespace WhateverDevs.SceneManagement.Runtime.AddressableManagement
         private IEnumerator CheckAvailableAddressablesRoutine(Action<AddressableStateReport> callback)
         {
             yield return new WaitForSeconds(1);
-            
+
             AddressableStateReport report = new AddressableStateReport();
-            
-            AsyncOperationHandle<IList<IResourceLocation>> handle = Addressables.LoadResourceLocationsAsync("Manifest");
 
-            yield return handle;
+            AsyncOperationHandle<IList<IResourceLocation>> manifestsHandle =
+                Addressables.LoadResourceLocationsAsync("Manifest");
 
-            foreach (IResourceLocation location in handle.Result)
+            yield return manifestsHandle;
+
+            Logger.Info("Found " + manifestsHandle.Result.Count + " manifest locations.");
+
+            foreach (IResourceLocation location in manifestsHandle.Result)
             {
-                Logger.Info("Checking " + location.PrimaryKey + "...");
+                Logger.Info("Checking " + location.PrimaryKey + " manifest...");
 
                 AsyncOperationHandle<AddressableManifest> manifestHandle =
                     Addressables.LoadAssetAsync<AddressableManifest>(location);
@@ -45,16 +56,20 @@ namespace WhateverDevs.SceneManagement.Runtime.AddressableManagement
 
                 if (manifest == null)
                 {
-                    Logger.Error(location.PrimaryKey + " is missing!");
+                    Logger.Error(location.PrimaryKey + " manifest is missing!");
                     report.AddressableStates[location.PrimaryKey] = AddressableState.Missing;
                 }
                 else
                 {
-                    Logger.Info(location.PrimaryKey + " found.");
+                    Logger.Info(location.PrimaryKey + " manifest found.");
+                    Logger.Info(manifest.name + " version is " + manifest.Version + ".");
+                    
+                    
+                    
                 }
             }
 
-            Addressables.Release(handle);
+            Addressables.Release(manifestsHandle);
         }
     }
 }
