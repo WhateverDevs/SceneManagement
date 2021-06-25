3fdf45947a6971187f3716700786c8aa83fb7762
Rodrigo Varga <varguiniano@gmail.com>
2021-06-25 20:09:27 +0200

    - Script to unload scenes on button click.

---
+++ Runtime/Ui/UnloadSceneOnButtonClick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.SceneManagement;
+using WhateverDevs.Core.Runtime.Ui;
+using WhateverDevs.SceneManagement.Runtime.SceneManagement;
+using Zenject;
+
+namespace WhateverDevs.SceneManagement.Runtime.Ui
+{
+    /// <summary>
+    /// Load the scene set on inspector on button click.
+    /// </summary>
+    public class UnloadSceneOnButtonClick : ActionOnButtonClick<LoadSceneOnButtonClick>
+    {
+        /// <summary>
+        /// Scene to be unloaded.
+        /// </summary>
+        public SceneReference SceneToUnload;
+
+        /// <summary>
+        /// Mode to load the scene into.
+        /// </summary>
+        public LoadSceneMode Mode;
+
+        /// <summary>
+        /// Reference to the scene manager.
+        /// </summary>
+        [Inject]
+        public ISceneManager SceneManager;
+
+        /// <summary>
+        /// Load the scene when the button is clicked.
+        /// </summary>
+        protected override void ButtonClicked() => SceneManager.UnloadScene(SceneToUnload, null, null);
+    }
+}
